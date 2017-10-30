a07df5361d6cb5ab6fdc8b0209735cf761d79c1c
Wang Denghan <wangdenghan@WangdeMac-mini.local>
2017-10-30 16:52:43 +0800

    122

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -8,36 +8,36 @@ use Auth;
 
 class SessionsController extends Controller
 {
-  public function create()
-  {
-      return view('sessions.create');
-  }
-
-  public function store(Request $request)
+    public function create()
     {
-       $this->validate($request, [
+        return view('sessions.create');
+    }
+
+    public function store(Request $request)
+    {
+        $this->validate($request, [
            'email' => 'required|email|max:255',
            'password' => 'required'
        ]);
 
-       $credentials = [
+        $credentials = [
            'email'    => $request->email,
            'password' => $request->password,
        ];
 
-       if (Auth::attempt($credentials, $request->has('remember'))) {
-           session()->flash('success', '欢迎回来！');
-           return redirect()->route('users.show', [Auth::user()]);
-       } else {
-           session()->flash('danger', '很抱歉，您的邮箱和密码不匹配');
-           return redirect()->back();
-       }
+        if (Auth::attempt($credentials, $request->has('remember'))) {
+            session()->flash('success', '欢迎回来！');
+            return redirect()->route('users.show', [Auth::user()]);
+        } else {
+            session()->flash('danger', '很抱歉，您的邮箱和密码不匹配');
+            return redirect()->back();
+        }
     }
 
-  public function destroy()
-   {
-       Auth::logout();
-       session()->flash('success', '您已成功退出！');
-       return redirect('login');
-   }
+    public function destroy()
+    {
+        Auth::logout();
+        session()->flash('success', '您已成功退出！');
+        return redirect('login');
+    }
 }
